07a9eafaafd0c4a52e80def1e031e7f2f76b118f
salahbltr <salaheddine.bellitir@gmail.com>
2023-11-25 15:24:02 -0800

    0x0B-ssh/

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,14 @@
-# edit config file
+# Client configuration file (w/ Puppet)
 include stdlib
 
-file_line { 'Turn off passwd':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '     PasswordAuthentication no',
-  replace => true,
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-file_line { 'identity change':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school',
-  replace => true,
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
-
